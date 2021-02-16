738d8466fed34cbc91eaed3917b0bae01a69f526
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2021-02-16 12:14:25 +0400

    Add React fibers

--- src/ReactRedo.js
+++ src/ReactRedo.js
@@ -16,23 +16,73 @@ const createTextElement = (text) => ({
   },
 });
 
-const render = (root, container) => {
+const createNode = (fiber) => {
   let node = null;
   node =
-    root.type === "TEXT_ELEMENT"
+    fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
-      : document.createElement(root.type);
+      : document.createElement(fiber.type);
 
-  Object.keys(root.props).forEach((prop) => {
-    if (prop !== "children") node[prop] = root.props[prop];
+  Object.keys(fiber.props).forEach((prop) => {
+    if (prop !== "children") node[prop] = fiber.props[prop];
   });
 
-  root.props.children.forEach((child) => {
-    render(child, node);
-  });
-
-  container.appendChild(node);
+  return node;
 };
+
+var nextUnitOfWork = null;
+
+const render = (element, container) => {
+  const unit = {
+    domNode: container,
+    children: [element, element],
+  };
+  nextUnitOfWork = unit;
+};
+
+function performUnitOfWork(unit) {
+  if (unit.parent) {
+    unit.parent.domNode.appendChild(unit.domNode);
+  }
+  if (unit.alreadyBeenUsed) {
+    if (unit.rightSibling) {
+      return unit.rightSibling;
+    }
+  } else {
+    unit.alreadyBeenUsed = true;
+    const elements = unit.children;
+    if (elements.length !== 0) {
+      let childrenUnitArray = [];
+      let lastRightSibling = null;
+      for (let i = elements.length - 1; i >= 0; --i) {
+        const nextUnit = {
+          domNode: createNode(elements[0]),
+          parent: unit,
+          rightSibling: lastRightSibling,
+          children: elements[i].props.children,
+        };
+        lastRightSibling = nextUnit;
+        childrenUnitArray[i] = nextUnit;
+      }
+      if (childrenUnitArray[0]) {
+        return childrenUnitArray[0];
+      }
+    }
+  }
+
+  return unit.parent;
+}
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  window.requestIdleCallback(workLoop);
+}
+
+window.requestIdleCallback(workLoop);
 
 const ReactRedo = {
   createElement,
